af57ac41207e9e83ca1af536c1b241a75c0dce8f
linndand <linnda.p.n@students.amikom.ac.id>
2023-06-25 13:07:12 +0700

    Abstraksi

---
+++ Abstraksi/Canon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraksi
+{
+    public class Canon : Printer
+    {
+        public override void print()
+        {
+            Console.WriteLine("Jenis printer : {0}", jenisPrinter);
+            Console.WriteLine("Display dimension {0}", show);
+            Console.WriteLine("Canon printer printing.....");
+        }
+    }
+}
---
+++ Abstraksi/LaserJet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraksi
+{
+    public class LaserJet : Printer
+    {
+        public override void print()
+        {
+            Console.WriteLine("Jenis printer : {0}", jenisPrinter);
+            Console.WriteLine("Display dimension {0}", show);
+            Console.WriteLine("LaserJet printer printing.....");
+        }
+    }
+}
